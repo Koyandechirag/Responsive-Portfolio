fe8a9a5d392ba0e3660d20867abe03203426f36a
Koyandechirag <140401753+Koyandechirag@users.noreply.github.com>
2024-01-22 21:36:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,8 +26,6 @@ window.onscroll =() =>{
         };
     });
 
-
-
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
@@ -58,3 +56,22 @@ ScrollReveal({
 // ScrollReveal().reveal('.home-img img, .skills-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
 ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwMMJ1G2gZmZBaRdI6hZ9JTR8af86JGk3BSMzVtxCbCB42nMOjhy9wwYwzvbIeBSixt/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      msg.innerHTML = "Message Sent Successfully"
+      setTimeout(function(){
+          msg.innerHTML = ""
+      },5000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
