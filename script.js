7d305351cce918843fbba4d0578c8c3f8be4781b
Koyandechirag <140401753+Koyandechirag@users.noreply.github.com>
2024-01-31 20:51:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ menuIcon.onclick = () => {
 }
 
 
-
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -35,6 +34,23 @@ navbar.classList.remove('active');
 
 };
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwMMJ1G2gZmZBaRdI6hZ9JTR8af86JGk3BSMzVtxCbCB42nMOjhy9wwYwzvbIeBSixt/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      msg.innerHTML = "Message Sent Successfully"
+      setTimeout(function(){
+          msg.innerHTML = ""
+      },5000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
 // darkmode 
 
 let darkModeIcon = document.querySelector('#darkMode-icon');
@@ -56,22 +72,3 @@ ScrollReveal({
 // ScrollReveal().reveal('.home-img img, .skills-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
 ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
-
-
-
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwMMJ1G2gZmZBaRdI6hZ9JTR8af86JGk3BSMzVtxCbCB42nMOjhy9wwYwzvbIeBSixt/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg");
-
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-      msg.innerHTML = "Message Sent Successfully"
-      setTimeout(function(){
-          msg.innerHTML = ""
-      },5000)
-      form.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
-})
